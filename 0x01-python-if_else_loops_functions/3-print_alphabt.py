151e03af6a6ddea7ea346e602ef858693c3559d3
TopBoyDrako <sinbadprince9@gmail.com>
2023-08-09 18:06:45 -0700

    3-print_alphabet.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 output = ""
-for i in range (97, 123):
+for i in range(97, 123):
     if (i != 101 and i != 113):
         output = "{}".format(chr(i))
         print(output, end="")
