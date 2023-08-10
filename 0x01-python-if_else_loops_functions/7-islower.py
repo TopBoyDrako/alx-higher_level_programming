8d74585157a7e2e02dd78c825c83a65062306a07
TopBoyDrako <sinbadprince9@gmail.com>
2023-08-10 16:43:03 -0700

    7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,18 +1,6 @@
 #!/usr/bin/python3
 
+# function checks for lowercase 
+
 def islower(c):
     return ord('a') <= ord(c) <= ord('z')
-
-def check_case(c):
-    case_dict = {
-        'lower': True,
-        'upper': False
-    }
-    return case_dict['lower' if islower(c) else 'upper']
-
-# Test cases
-characters = ['a', 'H', 'A', '3', 'g']
-
-for char in characters:
-    print("{} is {}".format(char, "lower" if check_case(char) else "upper"))
-
