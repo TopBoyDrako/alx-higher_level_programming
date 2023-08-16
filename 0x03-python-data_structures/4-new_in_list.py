2b16ab38bcc6f173074b1b564ca746e237892985
TopBoyDrako <sinbadprince9@gmail.com>
2023-08-16 02:23:34 -0700

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# this funcction replaces elements in a list without modifying the original
+
+def new_in_list(my_list, idx, element):
+
+    if my_list is None:
+        return None
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    my_list.copy()
+    my_list[idx] = element
