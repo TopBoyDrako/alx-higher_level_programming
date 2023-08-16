7369ba783e7ee2d137d807079f8cdd47a1bf426c
TopBoyDrako <sinbadprince9@gmail.com>
2023-08-16 06:09:38 -0700

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    if not sentence:
+        first_char = None
+    else:
+        first_char = sentence[0]
+    length = len(sentence)
+    return (length, first_char)
