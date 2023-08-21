f5fd2432a8fe2817d83466fcc3d1aa3a22b66fcf
TopBoyDrako <sinbadprince9@gmail.com>
2023-08-21 16:44:29 -0700

    103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -5,8 +5,8 @@ void print_python_list(PyObject *p) {
     int size, i;
     Py_ssize_t allocated;
 
-    if (PyList_Check(p)) {
-        size = PyList_Size(p);
+    if (p && PyObject_HasAttrString(p, "append")) {
+        size = (int)PyObject_Length(p);
         allocated = ((PyListObject *)p)->allocated;
 
         printf("[*] Python list info\n");
@@ -14,7 +14,8 @@ void print_python_list(PyObject *p) {
         printf("[*] Allocated = %zd\n", allocated);
 
         for (i = 0; i < size; i++) {
-            printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+            PyObject *element = PyList_GetItem(p, i);
+            printf("Element %d: %s\n", i, Py_TYPE(element)->tp_name);
         }
     }
 }
@@ -23,13 +24,17 @@ void print_python_bytes(PyObject *p) {
     Py_ssize_t size, i;
     unsigned char *data;
 
-    if (PyBytes_Check(p)) {
-        size = PyBytes_Size(p);
+    if (p && PyObject_HasAttrString(p, "decode")) {
+        size = PyObject_Length(p);
         data = (unsigned char *)PyBytes_AsString(p);
 
         printf("[.] bytes object info\n");
         printf("  size: %zd\n", size);
-        printf("  trying string: %s\n", PyUnicode_AsUTF8AndSize(p, NULL));
+        PyObject *decoded = PyObject_Repr(p);
+        PyObject *decoded_bytes = PyUnicode_AsEncodedString(decoded, "utf-8", "replace");
+        printf("  trying string: %s\n", PyBytes_AsString(decoded_bytes));
+        Py_XDECREF(decoded_bytes);
+        Py_XDECREF(decoded);
         printf("  first 10 bytes: ");
         for (i = 0; i < 10 && i < size; i++) {
             printf("%02x", data[i]);
@@ -57,11 +62,12 @@ int main(void) {
     print_python_bytes(b);
     print_python_list(l);
 
-    Py_DECREF(s);
-    Py_DECREF(b);
-    Py_DECREF(l);
+    Py_XDECREF(s);
+    Py_XDECREF(b);
+    Py_XDECREF(l);
 
     Py_Finalize();
 
     return (0);
 }
+
