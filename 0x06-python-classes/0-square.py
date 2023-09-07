8371acb87055cc27468fe291df14280a0d39cd76
TopBoyDrako <sinbadprince9@gmail.com>
2023-09-07 07:22:39 -0700

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -4,13 +4,14 @@
 This is a module
 """
 
+
 class Square:
-"""
-This class defines a square
-"""
+    """
+    This class defines a square
+    """
 
     def __init__(self):
+    """
+    This is an empty method
+    """
         pass
-"""
-This is an empty method
-"""
