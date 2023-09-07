81c1e1027907e614e6b7b4a4ac4dcde733bf7e9e
TopBoyDrako <sinbadprince9@gmail.com>
2023-09-07 08:28:44 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This module contains a class which defines a square with a  private size attribute.
+This module contains a class which defines a square with a
+private size attribute.
 """
 
 
