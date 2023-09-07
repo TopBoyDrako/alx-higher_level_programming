27eaa13459589fb467897401c41152bee0b2d206
TopBoyDrako <sinbadprince9@gmail.com>
2023-09-07 07:31:42 -0700

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,14 +3,13 @@
 This module contains an empty class which defines a square.
 """
 
-
 class Square:
     """
     This class defines a square
     """
 
     def __init__(self):
-    """
-    This is an empty method
-    """
+        """
+        This is an empty method
+        """
         pass
