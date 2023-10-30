bb436aec323580deca388f3dd4d057428e701d64
TopBoyDrako <sinbadprince9@gmail.com>
2023-10-30 18:53:00 +0100

    2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -59,11 +59,11 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """
-        The setter sets the height to make sure it is an        
+        The setter sets the height to make sure it is an
         integer
         """
         if not isinstance(value, int):
-            raise TypeError("height must be an integer")        
+            raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
