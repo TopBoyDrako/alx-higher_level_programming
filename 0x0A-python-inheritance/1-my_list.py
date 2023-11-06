b86ba91b296c2c90d0c44b95cad0cb9473d6ac87
TopBoyDrako <sinbadprince9@gmail.com>
2023-11-06 21:06:30 +0100

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+This module defines a class which inherits from another class.
+It also defines a function inside the calss that prints a sorted list in
+alphabetical order.
+"""
+
+
+class MyList(list):
+    """
+    This class defines a function that prints from the list in sorted ascending
+    order.
+    """
+
+    def print_sorted(self):
+        """
+        This functons prints a sorted list in ascending order
+        """
+
+        sorted_list = sorted(self)
+        print(sorted_list)
