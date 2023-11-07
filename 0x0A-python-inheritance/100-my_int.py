37d3d036c1c93b9c22a10f150401d829f5c4fd20
TopBoyDrako <sinbadprince9@gmail.com>
2023-11-07 02:34:48 +0100

    100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+This module contains a subclass inheriting from attributes and methods
+from it's parent class
+"""
+
+
+class MyInt(int):
+    """
+    This class is a rebel. It has the operators == and != inverted
+    """
+
+    def __eq__(self, value):
+        """Override == opeartor with != behavior."""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value
