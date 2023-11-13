1bb2438f14d61f014dedf8bf932e7e389478b452
TopBoyDrako <sinbadprince9@gmail.com>
2023-11-13 20:39:36 +0100

    1-main.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+from models.base import Base
+"""
+This module defines a subclass Rectangle that inherits from Base
+the superclass. It contains 4 different private attributes used to
+define the rectangle shape
+"""
+
+
+class Rectangle(Base):
+    """
+    This class defines a rectangle with 4 private attributes
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes the private attributes of the class rectangle"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+            return self.__width
+    @width.setter
+    def width(self, value):
+        if not isinstance (value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("Width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Height must be an integer")
+        if value < 0:
+            raise ValueError("Height must be >= 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
