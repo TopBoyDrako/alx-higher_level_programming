0d9b0a295af30cfd65547ae87bd4ea60b79c5d3b
TopBoyDrako <sinbadprince9@gmail.com>
2023-11-15 15:55:58 +0100

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -65,38 +65,13 @@ class Base:
         return dummy
 
     @classmethod
-    def save_to_file_csv(cls, list_objs):
-        '''Saves object to csv file.'''
-        from models.rectangle import Rectangle
-        from models.square import Square
-        if list_objs is not None:
-            if cls is Rectangle:
-                list_objs = [[o.id, o.width, o.height, o.x, o.y]
-                             for o in list_objs]
-            else:
-                list_objs = [[o.id, o.size, o.x, o.y]
-                             for o in list_objs]
-        with open('{}.csv'.format(cls.__name__), 'w', newline='',
-                  encoding='utf-8') as f:
-            writer = csv.writer(f)
-            writer.writerows(list_objs)
-
-    @classmethod
-    def load_from_file_csv(cls):
-        '''Loads object to csv file.'''
-        from models.rectangle import Rectangle
-        from models.square import Square
-        ret = []
-        with open('{}.csv'.format(cls.__name__), 'r', newline='',
-                  encoding='utf-8') as f:
-            reader = csv.reader(f)
-            for row in reader:
-                row = [int(r) for r in row]
-                if cls is Rectangle:
-                    d = {"id": row[0], "width": row[1], "height": row[2],
-                         "x": row[3], "y": row[4]}
-                else:
-                    d = {"id": row[0], "size": row[1],
-                         "x": row[2], "y": row[3]}
-                ret.append(cls.create(**d))
-        return ret
+    def load_from_file(cls):
+        """Returns a list of instances from a JSON file."""
+        filename = "{}.json".format(cls.__name__)
+        try:
+            with open(filename, "r", encoding="utf-8") as file:
+                json_string = file.read()
+                list_of_dicts = cls.from_json_string(json_string)
+                return [cls.create(**d) for d in list_of_dicts]
+        except FileNotFoundError:
+            return []
