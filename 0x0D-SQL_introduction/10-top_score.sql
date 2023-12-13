134a9f4d3e3ea8acf47abdb84d3ed3283931cb9e
TopBoyDrako <sinbadprince9@gmail.com>
2023-12-13 19:51:52 +0100

    10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,3 @@
--- this script list all records of the second_tbale in a particular order
+-- this script list all records of the second_table in a particular order
 
-SELECT score, name;
-FROM second_table;
+SELECT score, name FROM second_table;
