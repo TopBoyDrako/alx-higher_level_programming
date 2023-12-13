4cbd091d0375e0e1e4ecccaaaae0b34d98d2577b
TopBoyDrako <sinbadprince9@gmail.com>
2023-12-13 21:22:53 +0100

    11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,5 @@
 -- This script lists all records with a given criteria
 
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY DESC;
+SELECT score, name FROM second_table
+WHERE score >= 10;
+ORDER BY DESC;
