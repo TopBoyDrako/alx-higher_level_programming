9a9fa4c9f3cf9c71db2f9b7868528b5e4eb8624f
TopBoyDrako <sinbadprince9@gmail.com>
2023-12-15 01:05:34 +0100

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- This script lists all priveleges of the mysql users
 
-SHOW GRANTS FOR 'user_od_1'@'localhost';
-SHOW GRANTS FOR 'user_od_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
