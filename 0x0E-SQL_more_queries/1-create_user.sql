954bbd7a8b809c3793d42d38f772d604f2c209e9
TopBoyDrako <sinbadprince9@gmail.com>
2023-12-15 01:44:07 +0100

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,6 @@
 -- This script creates a new user and grants privileges
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'IDENTIFIED BY 'user_0d_1_pwq';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;	
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
