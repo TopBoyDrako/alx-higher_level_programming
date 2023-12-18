7d6d08ce6f692a447fcd09bd124b636b8c7ee702
TopBoyDrako <sinbadprince9@gmail.com>
2023-12-19 00:12:51 +0100

    10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- This script imports a database
+
+SELECT show.title, genre.genre_id
+FROM tv_shows AS show
+    INNER JOIN tv_show_genres AS genre
+    ON show.id = genre.show_id
+ORDER BY show.title, genre.genre_id;
