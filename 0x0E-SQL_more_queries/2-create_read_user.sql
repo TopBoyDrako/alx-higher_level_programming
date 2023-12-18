b0e0175ae2df6993ff48f4403219fb8741c021de
TopBoyDrako <sinbadprince9@gmail.com>
2023-12-18 14:28:41 +0100

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- This script creates a database and a user
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' 
-IDENTIFIED BY 'user_0d_2_pwd'
+IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
