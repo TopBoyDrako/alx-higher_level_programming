b0f82eed806db506ef09481f04c2f5e15766a166
TopBoyDrako <sinbadprince9@gmail.com>
2023-12-18 15:28:54 +0100

    4-never_empty.sql

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,8 +1,6 @@
 -- This script creates a table that makes sure id is not null
 
 CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT,
+    id INT DEFAULT 1,
     name VARCHAR(256)
 );
-INSERT INTO id_not_null (id)
-VALUE (1);
