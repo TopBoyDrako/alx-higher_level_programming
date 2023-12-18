a8df22fc8d7d1e39ba6bc29703a764913ddaa5ec
TopBoyDrako <sinbadprince9@gmail.com>
2023-12-18 20:46:07 +0100

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,9 @@
 -- This script creates a database and table with specifications
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT NOT NULL AUTO_INCREMENTED PRIMARY KEY,
     state_id INT NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states(id),
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id),
     name VARCHAR(256) NOT NULL
 );
