0ea15a3bddf535c2b4a3e89bd9a2f8d007463874
TopBoyDrako <sinbadprince9@gmail.com>
2023-12-19 23:07:06 +0100

    11-genre_id_all_shows.sql

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- This script lists all shows in the database
+
+SELECT tv_sho.title, genre.genre_id
+  FROM tv_shows AS tv_sho
+  LEFT JOIN tv_shows AS genre
+  ON tv_sho.id = genre.show_id
+ORDER BY sho.title, genre.genre_id;
