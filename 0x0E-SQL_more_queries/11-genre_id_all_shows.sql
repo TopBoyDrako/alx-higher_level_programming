820b8540a8b63d5bed5283c5adb0c4711854c14f
TopBoyDrako <sinbadprince9@gmail.com>
2023-12-19 23:10:20 +0100

    11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,6 +2,6 @@
 
 SELECT tv_sho.title, genre.genre_id
   FROM tv_shows AS tv_sho
-  LEFT JOIN tv_shows_genres AS genre
+  LEFT JOIN tv_show_genres AS genre
   ON tv_sho.id = genre.show_id
 ORDER BY sho.title, genre.genre_id;
