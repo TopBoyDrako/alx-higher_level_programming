3c71f2dcb472ee6b39ab17fa7d4c818147f71757
TopBoyDrako <sinbadprince9@gmail.com>
2023-12-21 22:07:02 +0100

    12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -5,4 +5,4 @@ FROM tv_shows AS tv_sho
   LEFT JOIN tv_show_genres AS genre
   ON tv_sho.id = genre.show_id
   WHERE genre.genre_id IS NULL
-ORDER BY tv_show.title, genre.genre_id
+ORDER BY tv_sho.title, genre.genre_id
