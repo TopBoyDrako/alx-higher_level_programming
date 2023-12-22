006dc690665f83c730bf27a90a3283fc029b3ff1
TopBoyDrako <sinbadprince9@gmail.com>
2023-12-22 13:59:44 +0100

    102-rating_shows.sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 --This script lists all shows by their rating.
 
-SELECT title, SUM(rate) AS rates
+SELECT title, SUM(`rate`) AS rates
 FROM tv_shows AS tv
   INNER JOIN tv_show_ratings AS tvrating
   ON tv.id = tvrating.show_id
