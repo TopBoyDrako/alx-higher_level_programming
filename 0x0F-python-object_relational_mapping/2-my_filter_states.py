a905750c6452bd54ad5936927df80542b5d4a445
TopBoyDrako <sinbadprince9@gmail.com>
2024-01-15 18:54:25 +0100

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This script takes in an argument and displays all it's values
+in the states table
+"""
+
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         password=argv[2], db=argv[3])
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY
+                   '{}'".format(argv[4]))
+
+    rows = cursor.fetchall()
+    for i in rows:
+        print(i)
+
+    cursor.close()
+    db.close()
