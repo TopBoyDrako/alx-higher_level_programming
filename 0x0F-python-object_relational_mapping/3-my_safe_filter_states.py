bdf8499bfcaa81ae3aa9342dc2943d60e7da0247
TopBoyDrako <sinbadprince9@gmail.com>
2024-01-15 19:45:41 +0100

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
                          password=argv[2], db=argv[3])
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE BIINARY name = %s", [argv[4]])
+    cursor.execute("SELECT * FROM states WHERE BINARY name = %s", [argv[4]])
 
     rows = cursor.fetchall()
     for i in rows:
