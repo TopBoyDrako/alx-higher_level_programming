eee256d4cd03841be261e8f90848c33221f11ebe
TopBoyDrako <sinbadprince9@gmail.com>
2024-01-15 20:18:32 +0100

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,9 +14,9 @@ if __name__ == "__main__":
                          password=argv[2], db=argv[3])
 
     cursor = db.cursor()
-    cursor.execute("SELET cities.id, cities.name, states.name FROM cities\
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities\
                     INNER JOIN states ON cities.state_id = states.id\
-                     ORDER BY cities.id ASC")
+                    ORDER BY cities.id ASC")
 
     rows = cursor.fetchall()
     for i in rows:
