99bd14e96c2343f430e24bdc57f3843c47a9cd0f
TopBoyDrako <sinbadprince9@gmail.com>
2024-01-19 11:29:07 +0100

    10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -11,7 +11,8 @@ from sys import argv
 
 if __name__ == "__main__":
     if len(argv) != 5:
-        print("Usage: {} < username > < password > < database > < state name >".format(argv[0]))
+        print("Usage: {} < username > < password > < database > < state name >"
+              .format(argv[0]))
         exit(1)
 
     username = argv[1]
