577633bf3a4b9df2f9a034f7da03641c962d88b1
TopBoyDrako <sinbadprince9@gmail.com>
2024-01-26 00:59:52 +0100

    4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # scipt takes in a url , sends a GET request to the url and displays body of the response
-curl -sX GET $1 -J "X-School-User-Id: 98" -L
+curl -s -H "X-School-User-Id: 98" "$1"
