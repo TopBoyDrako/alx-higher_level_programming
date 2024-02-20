28322a31f9a037e307241691222c6ac247e0c210
TopBoyDrako <sinbadprince9@gmail.com>
2024-02-20 01:20:33 +0100

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,7 +12,7 @@ import urllib.request
 if __name__ == "__main__":
     url = sys.argv[1]
     val = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+    data = urllib.parse.urlencode(val).encode("ascii")
 
     request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
